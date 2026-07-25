bbdb1dbe157d85ac3e81278c1992c696a5a1349d
Mohitthebad <mohit@thebadcompany.co>
2026-07-25 15:56:02 +0530

    Update email address in Contact component

--- src/Component/Contact.jsx
+++ src/Component/Contact.jsx
@@ -151,7 +151,7 @@ export default function Contact() {
                         href="mailto:krishansy1@gmail.com"
                       >
                         <span className="material-symbols-outlined text-[20px]">mail</span>
-                        contact@shiftquest.com
+                        krishan@shiftquest.com
                       </a>
                       <a
                         className="text-lg text-charcoal/80 hover:text-charcoal transition-colors flex items-center gap-4"
